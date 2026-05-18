db891c262ed1966b8ecdf48e107d08df9b84901d
ECHIBANI <amineechibani123@gmail.com>
2026-05-18 23:09:39 +0100

    Fix HTTPS middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->append(\App\Http\Middleware\TrustProxies::class);
     $middleware->append(\App\Http\Middleware\ForceHttps::class);
 
     $middleware->alias([
